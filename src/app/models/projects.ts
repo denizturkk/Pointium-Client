ab2bdeed7e73880c4a658f88396b01e053312668
Deniz Türk <denizturkk@Deniz-MacBook-Pro.local>
2022-08-18 20:26:59 +0300

    0.0

---
+++ src/app/models/projects.ts
@@ -0,0 +1,18 @@
+export interface Project {
+    Id:number
+    projectName:string;
+    customerId:number;
+    companyName:string;
+    explanation:string;
+    startedAt:Date;
+    status:boolean;
+    leadById:number;
+    leadByFirstName:string;
+    leadByLastName:string;
+    leadByEmail:string;
+    assignedById:number;
+    assignerFirstName:string;
+    assignerLastName:string;
+    assignerMail:string;
+
+}
